ffda84b46817b66d61e411ee81d9aafc0cf60b88
Sharif Rahaman <onlysumon@users.noreply.github.com>
2021-07-19 17:14:59 -0500

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+public class TwoSum {
+  
+  public int[] twoSumBruteForce(int[] nums, int target) {
+    // Time: O(n*n) Space: O(1)
+    for(int i = 0; i < nums.length; i++) {
+        for(int j = i + 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] == target) {
+                return new int[] {i, j};
+            }
+        }
+    }
+    return new int[]{};
+   }
+   
+   public int[] twoSumOptimized(int[] nums, int target) {
+        // Time: O(n) Space: O(n)
+        Map<Integer, Integer> numbersFound = new HashMap<>(nums.length);
+        numbersFound.put(nums[0], 0);
+        for(int i = 1; i < nums.length; i++) {
+            if(numbersFound.containsKey(target - nums[i])) {
+                return new int[]{ numbersFound.get(target - nums[i]), i };
+            }
+            numbersFound.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+}
